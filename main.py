bd788471428156c2a598972ad25d9c47274e9d52
KoteiNoAkuma <gugomes9@gmail.com>
2023-10-28 20:15:40 -0300

    Server receiving connection

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import socket
+import time
+import subprocess
+import threading
+
+
+def listen():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(("172.16.236.21", 6666))
+    conne, addr = sock.accept()
+    with conne:
+        print(f"Connected by {addr}")
+        while True:
+            data = conne.recv(1024)
+            if not data:
+                break
+            conne.sendall(data)
+
 
 if __name__ == '__main__':
-    print('PyCharm')
-
-
+    print('WELCOME TO WANYUDOU!! (THE GOOD SIDE)')
+    print("""
+      |_|   ,
+     ('.') ///
+     <(_)`-/'
+ <-._/J L /  -WANYUDOU-""")
+    listen()
